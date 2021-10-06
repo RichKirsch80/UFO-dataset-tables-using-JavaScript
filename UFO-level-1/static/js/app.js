9e2af5a39fe3df1d3ce0d98ad77e363bc0c5e5cb
RichKirsch80 <85321602+RichKirsch80@users.noreply.github.com>
2021-10-05 20:30:20 -0500

    updated ufo-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,5 @@
 //assign the data from data.js to variable
-var sightings = data;
+var tableData = data;
 
 //select the button
 var button = d3.select("#filter-btn");
@@ -11,7 +11,6 @@ var form = d3.select(".form-group");
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
-//complete the event handler function for the form
 function runEnter() {
     //prevent refreshing
     d3.event.preventDefault();
@@ -23,27 +22,67 @@ function runEnter() {
     var inputValue = inputElement.property("value");
     
     console.log(inputValue);
-    console.log(sightings);
 
-    var filteredData = sightings.filter(sightings => sightings.datetime === inputValue);
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
 
     console.log(filteredData);
 
-    for (row in filteredData) {
-        var list = d3.select("#ufo-table");
-        var date = filteredData.map(sightings => sightings.datetime);
-            list.ForEach.append("li").text(`${date}`);
-        var city = filteredData.map(sightings => sightings.city);
-            list.append("li").text(`${city}`);
-        var state = filteredData.map(sightings => sightings.state);
-            list.append("li").text(`${state}`);
-        var country = filteredData.map(sightings => sightings.country);
-            list.append("li").text(`${country}`);
-        var shape = filteredData.map(sightings => sightings.shape);
-            list.append("li").text(`${shape}`);
-        var comment = filteredData.map(sightings => sightings.comment);
-           list.append("li").text(`${comment}`); 
-        }
+    var results = {
+     dates: [],
+     cities: [],
+     states: [],
+     countries: [],
+     shapes: [],
+     durationMinutes: [],
+     comments: []
+    };
 
-   
-}
+    var keyMap = {
+      datetime: "dates",
+      city: "cities",
+      state: "states",
+      country: "countries",
+      shape: "shapes",
+      durationMinutes: "durationMinutes",
+      comment: "comments"
+    };
+
+    filteredData.forEach((filteredData) => {
+    Object.entries(filteredData).forEach(([key, value]) => {
+      if (key === "datetime") {
+        dates.push(value);
+      }
+      if (key === "city") {
+        cities.push(value);
+      }
+      if (key === "State") {
+          states.push(value);
+      }
+      if (key === "Country") {
+        countries.push(value);
+      }
+      if (key === "Shape") {
+          shapes.push(value);
+      }
+      if (key === "durationMinutes") {
+          durationMinutes.push(value);
+      }
+      else {
+          comment.push(value);
+      }
+     });
+  });
+} 
+
+
+
+    
+    
+
+
+
+
+
+
+
+
